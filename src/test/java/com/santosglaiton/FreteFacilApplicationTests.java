34a494d5add1929e524fee5d03670353ad6afc53
santosglaiton <7302zm2O>
2021-06-23 15:24:11 -0300

    Initial commit

---
+++ src/test/java/com/santosglaiton/FreteFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosglaiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
